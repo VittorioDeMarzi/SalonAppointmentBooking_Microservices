552e2e28764de4ce2247cf6ea74a18390df44f5b
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-02 17:18:41 +0200

    add eurecaClient dependencies to all poms + application.properties

--- services/booking_service/src/main/java/com/demadev/BookingServiceApplication.java
+++ services/booking_service/src/main/java/com/demadev/BookingServiceApplication.java
@@ -2,6 +2,7 @@ package com.demadev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
 
 @SpringBootApplication
 public class BookingServiceApplication {
