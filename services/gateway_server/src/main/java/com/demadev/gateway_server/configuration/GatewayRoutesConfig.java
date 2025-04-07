dc11a963291091b725f8850c0e6683b993ae0c20
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-07 19:26:17 +0200

    add CustomRoutesConfig

---
+++ services/gateway_server/src/main/java/com/demadev/gateway_server/configuration/GatewayRoutesConfig.java
@@ -0,0 +1,41 @@
+package com.demadev.gateway_server.configuration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Booking Service
+                .route("booking-service", r -> r
+                        .path("/api/v1/bookings/**")
+                        .uri("lb://BOOKING-SERVICE")
+                )
+                // Payment Service
+                .route("payment-service", r -> r
+                        .path("/api/v1/payments/**")
+                        .uri("lb://PAYMENT-SERVICE")
+                )
+                .route("category-service", r -> r
+                        .path("/api/v1/categories/**", "/api/v1/categories/salon-owner/**")
+                        .uri("lb://CATEGORY-SERVICE")
+                )
+                .route("salon-service", r -> r
+                        .path("/api/v1/salons/**", "/api/v1/admin/salons")
+                        .uri("lb://SALON-SERVICE")
+                )
+                .route("service-offering", r -> r
+                        .path("/api/v1/service-offering/**")
+                        .uri("lb://SERVICE-OFFERING")
+                )
+                .route("user-service", r -> r
+                        .path("/api/v1/user**")
+                        .uri("lb://USER-SERVICE")
+                )
+                .build();
+    }
+}
