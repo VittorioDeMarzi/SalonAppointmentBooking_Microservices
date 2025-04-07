725718d2795906b8d9199f75f0169e3604c159d8
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-07 19:25:01 +0200

    modify names services

--- services/gateway_server/src/main/java/com/demadev/gateway_server/GatewayServerApplication.java
+++ services/gateway_server/src/main/java/com/demadev/gateway_server/GatewayServerApplication.java
@@ -2,6 +2,7 @@ package com.demadev.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
 
 @SpringBootApplication
 public class GatewayServerApplication {
